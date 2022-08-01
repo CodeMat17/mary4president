5bdf051992edd87d2882f2625ab960656a536e6f
Matthew <matgold17@gmail.com>
2022-08-01 19:25:04 +0100

    added owners link

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,8 @@
+import Link from 'next/link';
+
 function Footer() {
   return (
-    <div className='bg-lime-800 px-8 py-6 flex justify-center '>
+    <div className='bg-lime-800 px-8 py-6 flex flex-col justify-center '>
       <p className='text-sm text-center text-white/50 max-w-2xl mx-auto'>
         For your website, portfolio page, online store (e-commerce), e-diary,
         finance tracking or banking apps, school and hotel management software
@@ -11,6 +13,13 @@ function Footer() {
           here.
         </a>
       </p>
+      <code className='text-center text-sm pt-4 text-lime-500/90'>
+        Courtesy:{' '}
+        <Link href='https://matthewchukwu.dev'>
+          <a className='text-amber-500 hover:underline'>Matthew Chukwu</a>
+        </Link>
+        , Calabar Centre.
+      </code>
     </div>
   );
 }
